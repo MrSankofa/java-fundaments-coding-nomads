04b615e9ec9b8edd3a3a18c21459f88356566e9e
brett.cunningham <brett.cunningham@sbevision.com>
2024-03-17 18:10:36 -0400

    completed(exercise): objects ex #3 on getters setters, to string and instance vairables

--- src/labs_examples/objects_classes_methods_target/labs/objects/Exercise_01.java
+++ src/labs_examples/objects_classes_methods_target/labs/objects/Exercise_01.java
@@ -1,8 +1,26 @@
 package labs_examples.objects_classes_methods_target.labs.objects;
 
+import java.util.ArrayList;
+
 public class Exercise_01 {
   public static void main(String[] args) {
     Airplane my747 = new Airplane(new Pilot("Brett", 37), new Company("Boeing", 1983));
+
+    my747.pilot.setName("Brett Cunningham II");
+    System.out.println("How old is Brett? " + my747.pilot.getAge());
+
+
+    my747.amenities.entertainment.add("Movies");
+    my747.amenities.entertainment.add("Internet");
+    my747.amenities.entertainment.add("Live TV");
+
+    PlaneEngine pEngine = new PlaneEngine();
+    pEngine.setBrand("Chevrolet");
+    pEngine.setName("V8");
+
+    my747.setEngine(pEngine);
+
+    System.out.println("Here is my 747: " + my747);
   }
 
 }
@@ -11,12 +29,46 @@ public class Exercise_01 {
 class Airplane {
   Pilot pilot;
   Company company;
+
+
+
+  Amenities amenities = new Amenities();
+  PlaneEngine engine = new PlaneEngine();
+
   double fuelCapacity;
   double currentFuelLevel;
 
   Airplane(Pilot pilot, Company company) {
     this.pilot = pilot;
     this.company = company;
+  }
+
+  public Amenities getAmenities() {
+    return amenities;
+  }
+
+  public void setAmenities(Amenities amenities) {
+    this.amenities = amenities;
+  }
+
+  public PlaneEngine getEngine() {
+    return engine;
+  }
+
+  public void setEngine(PlaneEngine engine) {
+    this.engine = engine;
+  }
+
+  @Override
+  public String toString() {
+    return "Airplane{" +
+        pilot +
+        company +
+        amenities +
+        engine +
+        fuelCapacity +
+        currentFuelLevel +
+        '}';
   }
 }
 
@@ -45,6 +97,14 @@ class Pilot {
 
   public void setAge(int age) {
     this.age = age;
+  }
+
+  @Override
+  public String toString() {
+    return "Pilot{" +
+        "name='" + name + '\'' +
+        ", age=" + age +
+        '}';
   }
 }
 
@@ -76,5 +136,83 @@ class Company {
   public int getSince() {
     return since;
   }
+
+  @Override
+  public String toString() {
+    return "Company{" +
+        "since=" + since +
+        ", name='" + name + '\'' +
+        '}';
+  }
 }
+
+class PlaneEngine {
+
+
+  private String name;
+  private String Brand;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getBrand() {
+    return Brand;
+  }
+
+  public void setBrand(String brand) {
+    Brand = brand;
+  }
+
+  @Override
+  public String toString() {
+    return "PlaneEngine{" +
+        "name='" + name + '\'' +
+        ", Brand='" + Brand + '\'' +
+        '}';
+  }
+}
+
+class Amenities {
+  ArrayList<String> entertainment = new ArrayList<String>();
+
+  @Override
+  public String toString() {
+    return "Amenities{" +
+        "entertainment=" + entertainment +
+        '}';
+  }
+}
+
+// exercise 3
+class Hanger {
+  private String bay;
+
+  public String getBay() {
+    return bay;
+  }
+
+  public void setBay(String bay) {
+    this.bay = bay;
+  }
+}
+
+// (DONE) exercise 1 asks for an airplane class composed of 4 other classes
+// (DONE) exercise 1 asks for an airplane class composed of 4 other classes
+
+
+// (DONE) exercise 2 asks you to change the instance variables to private and create get/set for each ins var
+// (DONE) also create a toString method in each POJO
+// (DONE) demo getting the variables from the class you composed the airplane object and invoking the to string methods
+
+
+// exercise 3 - demo object association in the Exercise_01 controller class (it contains the main() method)
+// create at least 2 POJOs
+// associate the 2 objects together
+
+
 
